0739ab4ff5f6b985b175275fc4955a3958736c05
Admin <Admin@DESKTOP-Q79K2ON>
2026-03-28 17:01:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    std::cout << std::fixed << std::setprecision(6);
+    std::cout << "=========== Задание а) ========" << std::endl;
+
+    double expr_a = sqrt(4.12 + log(2)) - sin(0.5) + atan(-3.0 / 4.0);
+    std::cout << "sqrt(4.12 + ln2) - sin(1/2) + arctg(-3/4) = " << expr_a << std::endl;
+    std::cout << std::endl;
+
+    return 0;
+}
