d513a0818d1cab202ec487180849c215e372b973
apssd7696-byte <apssd7696@gmail.com>
2026-05-05 13:18:11 +0500

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "");
-    cout << std::fixed << setprecision(6);
+    cout << fixed << setprecision(6);
     double rez = pow(4.12 + log(2), 1.0 / 3.0) - sin(0.5) + atan(-0.75);
     cout << "Результат:" << rez << endl;
 }
